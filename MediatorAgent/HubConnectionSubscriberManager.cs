d47d516bbf339ee65cee2aa78a7e3c370b9441de
Gaurav Narula <gnarula94@gmail.com>
2020-03-10 12:13:29 +0100

    Create queues for every inbox and subscribe to it when client connects
    Uses BlockingCollection as the underlying in-memory queue

---
+++ MediatorAgent/HubConnectionSubscriberManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace MediatorAgent
+{
+    public class HubConnectionSubscriberManager
+    {
+        /**
+         * Stores a map from SignalR connection id to reference of IDisposables
+         * that stop the subscription to the corresponding inbox
+         */
+        public ConcurrentDictionary<string, IDisposable> connectionDisposableMap = new ConcurrentDictionary<string, IDisposable>();
+
+        /**
+         * Associates a SignalR connection id to a IDisposable
+         */
+        public void AssociateDisposable(string connectionId, IDisposable disposable)
+        {
+            connectionDisposableMap.TryAdd(connectionId, disposable);
+        }
+
+        /**
+         * Stops the subscriber associated with the connection id
+         */
+        public void StopMonitoringMessages(string connectionId)
+        {
+            connectionDisposableMap.TryRemove(connectionId, out IDisposable value);
+            value.Dispose();
+            System.Diagnostics.Debug.WriteLine("Disposed subscriber for " + connectionId);
+        }
+    }
+}
--- MediatorAgent/MediatorHub.cs
+++ MediatorAgent/MediatorHub.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using System.Threading.Tasks;
 using Hyperledger.Aries.Agents;
 using Hyperledger.Aries.Extensions;
@@ -14,73 +17,42 @@ namespace MediatorAgent
 {
     public class MediatorHub : Hub
     {
-        private IAgentProvider _agentProvider;
-        private IConnectionService _connectionService;
-        private IWalletRecordService _walletRecordService;
-        private IWalletService _walletService;
+        
+        private HubMethods _hubMethods;
+        private HubConnectionSubscriberManager _hubConnectionSubscriberManager;
 
-        public MediatorHub(IAgentProvider agentProvider, IConnectionService connectionService, IWalletRecordService walletRecordService, IWalletService walletService)
+        public MediatorHub(HubMethods hubMethods, HubConnectionSubscriberManager hubConnectionSubscriberManager)
         {
-            _agentProvider = agentProvider;
-            _connectionService = connectionService;
-            _walletRecordService = walletRecordService;
-            _walletService = walletService;
+            
+            _hubMethods = hubMethods;
+            _hubConnectionSubscriberManager = hubConnectionSubscriberManager;
         }
 
         public override Task OnConnectedAsync()
         {
-            UserManager.addConnection(Context.ConnectionId);
             Clients.Caller.SendAsync("Authorize", Context.ConnectionId);
             return base.OnConnectedAsync();
         }
 
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            UserManager.removeConnection(Context.ConnectionId);
+            System.Diagnostics.Debug.WriteLine("Disconnected. Removing observable...");
+            _hubConnectionSubscriberManager.StopMonitoringMessages(Context.ConnectionId);
             return base.OnDisconnectedAsync(exception);
         }
 
         public async Task AuthorizeResponse(string message)
         {
             System.Diagnostics.Debug.WriteLine("Received AuthorizeResponse");
-
-            var (authorizeResponse, connection) = await GetMessage<AuthorizeResponseMessage>(message);
-            System.Diagnostics.Debug.WriteLine("Nonce Received: " + authorizeResponse.Nonce + " Expected " + Context.ConnectionId);
-            if (authorizeResponse.Nonce == Context.ConnectionId)
-            {
-                UserManager.associateConnection(authorizeResponse.Nonce, connection.TheirDid);
-                UserManager.Print();
-            }
+            await _hubMethods.handleAuthorizeResponse(message, Context.ConnectionId);
             System.Diagnostics.Debug.WriteLine("Processed AuthorizeResponse");
         }
 
         public async Task Acknowledge(string message)
         {
             System.Diagnostics.Debug.WriteLine("Received Acknowledgement");
-            var agentContext = await _agentProvider.GetContextAsync();
-            var (acknowledgement, connection) = await GetMessage<AcknowledgeMessage>(message);
-
-            var inboxId = connection.GetTag("InboxId");
-            var inboxRecord = await _walletRecordService.GetAsync<InboxRecord>(agentContext.Wallet, inboxId);
-            var edgeWallet = await _walletService.GetWalletAsync(inboxRecord.WalletConfiguration, inboxRecord.WalletCredentials);
-            try
-            {
-                await _walletRecordService.DeleteAsync<InboxItemRecord>(edgeWallet, acknowledgement.ItemId);
-            } catch(Exception)
-            {
-                System.Diagnostics.Debug.WriteLine("Couldn't delete inbox item with id: " + acknowledgement.ItemId);
-            }
+            await _hubMethods.handleAcknowledge(message);
             System.Diagnostics.Debug.WriteLine("Processed Acknowledgement");
-        }
-
-        private async Task<(T, ConnectionRecord)> GetMessage<T>(string message) where T : AgentMessage, new() {
-            var agentContext = await _agentProvider.GetContextAsync();
-            var unpackedMessage = await Crypto.UnpackMessageAsync(agentContext.Wallet, message.GetUTF8Bytes());
-            var unpackResult = unpackedMessage.ToObject<UnpackResult>();
-            var connection = await _connectionService.ResolveByMyKeyAsync(agentContext, unpackResult.RecipientVerkey);
-            var unpackedMessageContext = new UnpackedMessageContext(unpackResult.Message, connection);
-
-            return (unpackedMessageContext.GetMessage<T>(), connection);
         }
 
     }
